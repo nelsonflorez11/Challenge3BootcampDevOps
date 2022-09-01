83e38e46a962747b8f4038b95f83a024a4db13bd
Nelson Florez <nelsonflorez11@gmail.com>
2022-08-31 22:42:27 -0500

    ADD: Issue #8: Bonus 8 02

--- reto8/README.md
+++ reto8/README.md
@@ -1,1 +1,33 @@
-# Reto 8 
+# Reto 8 - Servidor web
+
+###### Nota: se realizo un script que automatiza los pasos
+
+1. Clona el repositorio de https://github.com/roxsross/pokepy en esta ubicación
+
+2.    Muévete al directorio a través de la CLI y ejecuta el comando docker build para construir una nueva imágen de Docker en base al Dockerfile existente en el repositorio, por ejemplo docker build -t roxsross12/pokepy-ejemplo:1.0 ., donde roxsross12 es el nombre de usuario en DockerHub, pokepy-ejemplo es el nombre que le vas a poner a tu imagen (puede ser cualquiera, el que tu gustes), y 1.0 la versión de tu imagen (puedes utilizar cualquier versión para este ejemplo)
+
+3. Utiliza el comando docker images para que puedas listar todas las imágenes existentes en tu computadora o máquina host. Si te das cuenta, la nueva imagen que acabas de construir debería estar listada ahí
+
+4. Instancia un contenedor en base a esa nueva imagen utilizando docker run
+
+5. Verifica que tu contenedor está corriendo correctamente por medio de http://localhost:5000/
+
+6. Esta imágen existe solamente en tu máquina host, puedes proceder a subirla a tu cuenta de docker hub utilizando el comando docker push o bien, borrarla de tu máquina local con docker rmi <image-name>
+
+
+###### Solucion de los puntos anteriores
+![Screenshot](./img/reto8_0.png)
+<br>
+---
+
+![Screenshot](./img/reto8_1.png)
+<br>
+---
+
+![Screenshot](./img/reto8_2.png)
+<br>
+---
+
+![Screenshot](./img/reto8_3.png)
+<br>
+---
--- reto8/reto-8.sh
+++ reto8/reto-8.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#correr export desde la misma shell que ejecute el script, remplazando XXXXXXXX como corresponda 
+
+#export user_docker=XXXXXXXX
+#export passwd_docker=XXXXXXXX
+
 PS3='Please enter your choice: '
 options=(
         "Iniciar"
@@ -15,17 +20,23 @@ do
             echo Repositorio Descargado""
             echo "########################"
             cd pokepy 
-            docker build -t neslonflorez11/pokepy:1.0 .
+            docker build -t nelsonflorez11/pokepy:1.0 .
             docker images
             echo "########################"
             echo "Imagen neslonflorez11/pokepy:1.0 creada"
             echo "#########################"
-            docker run -d --name web -p 5000:5000 neslonflorez11/pokepy:1.0
+            docker run -d --name web -p 5000:5000 nelsonflorez11/pokepy:1.0
             docker ps
             echo "########################"
             echo "Contenedor creado"
             echo "ingresar a URL http://localhost:5000/"
-            echo "#########################";;
+            echo "#########################"
+            docker login -u $user_docker -p $passwd_docker docker.io                
+            docker push nelsonflorez11/pokepy:1.0
+            echo "########################"
+            echo "Imagen subida a Docker Hub"            
+            echo "#########################"
+            ;;
         "Eliminar recursos")
             cd src
             cd ..          
@@ -33,6 +44,9 @@ do
             docker stop $(docker ps -a -q)
             docker rm $(docker ps -a -q)
             docker rmi -f $(docker images -a -q)
+            echo "########################"
+            echo "Se borraron todos los contenedores e imagenes"            
+            echo "#########################"
             ;;            
         "Quit") 
             break ;;
